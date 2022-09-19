67ca2298f80efc4737a77022f34ce9dc9d0250ae
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-09-19 07:49:44 -0700

    created sidebar components

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,13 +5,13 @@ import {BellIcon, BookmarkIcon, ClipboardIcon, DotsCircleHorizontalIcon, DotsHor
 
 export default function Sidebar() {
   return (
-    <div>
-      <div className="">
+    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
+      <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
         <Image width="50" height="50" src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"></Image>
       </div>
       {/* menu */}
-      <div className="">
-        <SidebarMenuItem text="Home" Icon={HomeIcon}/>
+      <div className="mt-4 mb-2.5 xl:items-start">
+        <SidebarMenuItem text="Home" Icon={HomeIcon} active/>
         <SidebarMenuItem text="Explore" Icon={HashtagIcon}/>
         <SidebarMenuItem text="Notifications" Icon={BellIcon}/>
         <SidebarMenuItem text="Messages" Icon={InboxIcon}/>
@@ -20,16 +20,16 @@ export default function Sidebar() {
         <SidebarMenuItem text="Profile" Icon={UserIcon}/>
         <SidebarMenuItem text="More" Icon={DotsCircleHorizontalIcon}/>
       </div>
-      <button>Tweet</button>
+      <button className="bg-blue-400 text-white rounded-full w-56 h-12 font-bold shadow-md hover: brightness-95 text-lg hidden xl:inline">Tweet</button>
 
-      <div>
-        <img src="https://nuelportfolio.netlify.app/static/media/me-about.5cea161175f687029ed3.png" alt="user Image" />
-        <div className="">
-          <h4>Nuel Akinsiku</h4>
-          <p>Frontend Dev</p>
+      <div className="hoverEffect  text-gray-700 flex items-center justify-center xl:justify-start mt-auto">
+        <img className="h-10 w-10 rounded-full xl:mr-2" src="https://nuelportfolio.netlify.app/static/media/me-about.5cea161175f687029ed3.png" alt="user Image" />
+        <div className="leading-5 hidden xl:inline">
+          <h4 className="font-bold">Nuel Akinsiku</h4>
+          <p className="text-gray-500">Frontend Dev</p>
         </div>
+        <DotsHorizontalIcon className="h-5 xl:ml-8 hidden xl:inline"/>
       </div>
-      <DotsHorizontalIcon />
     </div>
   );
 }

f73c81ae8dff2844d71f33a318234d7378b7ac36
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-09-19 12:50:35 -0700

    created field components and it's header

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import Head from 'next/head'
+import Feed from '../components/Feed'
 import Sidebar from '../components/Sidebar'
 
 
@@ -16,6 +17,7 @@ export default function Home() {
 
       {/* Feed */}
 
+      <Feed />
 
       {/* Widgets */}
 

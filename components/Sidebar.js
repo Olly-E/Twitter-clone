77b6526167f63f06c637e0d1d42ba25debed3d10
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-09-22 14:26:36 -0700

    created news feed of the wodget components

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -5,7 +5,7 @@ import {BellIcon, BookmarkIcon, ClipboardIcon, DotsCircleHorizontalIcon, DotsHor
 
 export default function Sidebar() {
   return (
-    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full">
+    <div className="hidden sm:flex flex-col p-2 xl:items-start fixed h-full xl:ml-24">
       <div className="hoverEffect p-0 hover:bg-blue-100 xl:px-1">
         <Image width="50" height="50" src="https://help.twitter.com/content/dam/help-twitter/brand/logo.png"></Image>
       </div>

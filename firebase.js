e66a97dacce9c7bbd7e904b5b9df2f8ea432e008
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-09-25 06:52:11 -0700

    complet signing page with google auth

--- firebase.js
+++ firebase.js
@@ -7,7 +7,7 @@ import {getStorage} from 'firebase/storage'
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: process.env.FIREBASE_API_KEY,
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
   authDomain: "twitter-5f5ca.firebaseapp.com",
   projectId: "twitter-5f5ca",
   storageBucket: "twitter-5f5ca.appspot.com",
--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -9,5 +9,8 @@ export const authOptions = {
     }),
     // ...add more providers here
   ],
+  pages: {
+    signin:"/auth/signin"
+  }
 }
 export default NextAuth(authOptions)

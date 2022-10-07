e5e6af84e8a04164d47ec82afe6d63e0ea8e2b85
Emmanuel Akinsiku <akinsnuel@gmail.com>
2022-10-07 14:17:11 -0700

    fixed secret and browser compatibility error

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,6 +12,9 @@ export const authOptions = {
   pages: {
     signIn:"/auth/signin"
   },
+
+  secret: process.env.SECRET,
+
   callbacks: {
     async session({session, token}){
         session.user.username = session.user.name
